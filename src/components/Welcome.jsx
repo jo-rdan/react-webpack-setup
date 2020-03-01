513ceac3f96a8ee3abb2eaaaad6e1d2d20452ec3
Jordan Kayinamura <jordankayinamura@gmail.com>
2020-03-01 18:33:39 +0200

    webpack configuration

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1> Welcome to my configured react app</h1>
+    </div>
+  );
+};
+
+export default Welcome;
